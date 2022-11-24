ea02754909568dd311eddc14b8ce2400f0a230d1
intfloatjohn <118879485+intfloatjohn@users.noreply.github.com>
2022-11-24 14:06:57 +0800

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,1 +1,21 @@
+#include"encrypt__.h"
+float encrypt(float x_1;float x_2;float y_1;float y_2);
+int main()
+{
+  
+  
+  
+  
+  
+  
+  
+}
+float encrypt(float x_1;float x_2;float y_1;float y_2)
+{//use Quadratic functions as the hash function
+    int xx1,xx2,yy1,yy2;
+  scanf("%f",&X_1);
+  
+  
+  
 
+}
